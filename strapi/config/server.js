247d58ead8f7a8907a3145e3a4500e1d61e27b85
Eric Oei <wcoei.144@gmail.com>
2020-10-04 23:57:20 +0800

    Dockerize strapi (with s3 integration preconfigured)

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('STRAPI_HOST', '0.0.0.0'),
+  port: env.int('STRAPI_PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '1a5904aa5752ab3c717bcc3079fa3f96'),
