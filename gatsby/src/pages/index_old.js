bd73b4c91b8c329e237690ea31bb14c466d1ca44
Wing Chiu Oei <wcoei@WCOEI-MACBOOKPRO.local>
2020-10-05 23:07:22 +0800

    Generate blog pages from strapi

--- gatsby/src/pages/index_old.js
+++ gatsby/src/pages/index_old.js
@@ -6,8 +6,8 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-  const posts = data.allStrapiArticle.nodes
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const posts = data.allMarkdownRemark.nodes
 
   if (posts.length === 0) {
     return (
@@ -27,12 +27,12 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
-      <h1>test2</h1>
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
-          const title = post.Subject
+          const title = post.frontmatter.title || post.fields.slug
+
           return (
-            <li key={post.Slug}>
+            <li key={post.fields.slug}>
               <article
                 className="post-list-item"
                 itemScope
@@ -40,16 +40,16 @@ const BlogIndex = ({ data, location }) => {
               >
                 <header>
                   <h2>
-                    <Link to={"/"+post.Slug} itemProp="url">
+                    <Link to={post.fields.slug} itemProp="url">
                       <span itemProp="headline">{title}</span>
                     </Link>
                   </h2>
-                  <small>{post.createAt}</small>
+                  <small>{post.frontmatter.date}</small>
                 </header>
                 <section>
                   <p
                     dangerouslySetInnerHTML={{
-                      __html: post.Content,
+                      __html: post.frontmatter.description || post.excerpt,
                     }}
                     itemProp="description"
                   />
@@ -72,13 +72,17 @@ export const pageQuery = graphql`
         title
       }
     }
-    allStrapiArticle {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       nodes {
-        id
-        Slug
-        Subject
-        Content
-        createdAt
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+        }
       }
     }
   }
