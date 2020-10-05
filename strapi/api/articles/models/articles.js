f873bc22bd9b6bbe1f1f996d54ffa11e94c9b8e6
Eric Oei <wcoei.144@gmail.com>
2020-10-05 19:37:00 +0800

    Enable Articles Slug 2nd commit

--- strapi/api/articles/models/articles.js
+++ strapi/api/articles/models/articles.js
@@ -1,8 +1,18 @@
-'use strict';
-
+const slug = require('slug');
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        beforeCreate: async (data) => {
+            if (data.Subject) {
+              data.Slug = slug(data.Subject);
+            }
+        },
+        beforeUpdate: async (params, data) => {
+            data.Slug = slug(data.Subject);
+        },
+    },
+};
